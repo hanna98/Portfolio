8da45703d1b16f4719c2e4e3687775c5ba1d6c16
hanna98 <hanna.marohn@gmail.com>
2024-01-11 11:59:52 +0100

    La till en product list item

---
+++ src/components/ProductListItem.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const Box = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: pink;
+`
+
+
+function ProductListItem () {
+    return (
+        <Box>
+            product list item
+        </Box>
+        
+    )
+}
+
+export default ProductListItem
